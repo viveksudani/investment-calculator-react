d66212ef738413c495aa2014ff86d39adee316a5
Vivek Sudani <viveksudani7@gmail.com>
2023-07-25 10:14:58 +0530

    split code into components

---
+++ src/components/InvestmentResults/InvestmentResults.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import styles from "./InvestmentResults.module.css";
+
+const InvestmentResults = () => {
+  return (
+    //   {/* Todo: Show below table conditionally (only once result data is available) */}
+    //   {/* Show fallback text if no data is available */}
+
+    <table className={styles.result}>
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Total Savings</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>YEAR NUMBER</td>
+          <td>TOTAL SAVINGS END OF YEAR</td>
+          <td>INTEREST GAINED IN YEAR</td>
+          <td>TOTAL INTEREST GAINED</td>
+          <td>TOTAL INVESTED CAPITAL</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default InvestmentResults;
